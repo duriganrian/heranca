08ebc4a257a69218e9d55231f335fbee153aa9b3
duriganrian <126262350+duriganrian@users.noreply.github.com>
2023-10-06 16:31:31 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Assalariado } from "./model/Assalariado";
 import { comissionado } from "./model/Comissionado";
+import { Horista } from "./model/Horista";
 
 const a1 = new Assalariado(`Miroslav klose`, `12345678989`, 1500);
 
@@ -20,7 +21,14 @@ console.log(`Nome: ${c1.getNome()}, CPF: ${c1.getCpf()}`);
 console.log(`Comissao: ${c1.getTaxaComissao()} Total Vendas: ${c1.getTotalVendas()}`);
 console.log(`Vencimento: ${c1.vencimento()}`);
 
-
+const h1 = new Horista ('oloco', '12345676789', 59, 19)
+h1.setNome(`kovaslk`);
+h1.setCpf(`660-332-974-90`);
+h1.setValorHora(30);
+h1.setHorasTrabalhadas(9);
+console.log(`Nome: ${h1.getNome()}, CPF: ${h1.getCpf()}`);
+console.log(`Valor da Hora: ${h1.getValorHora()}, Horas Trabalhadas: ${h1.getHorasTrabalhadas()}`);
+console.log(`Vencimento: ${h1.vencimento()}`);
 
 
 
